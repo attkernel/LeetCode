9a23afb6ccff6503bb92d200ba34db20f4fcc1cb
attkernel <attkernel@gmail.com>
2020-05-09 21:34:13 +0800

    add file

---
+++ 217_containsDuplicate.go
@@ -0,0 +1,44 @@
+/**
+给定一个整数数组，判断是否存在重复元素。
+
+如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+
+
+示例 1:
+
+输入: [1,2,3,1]
+输出: true
+示例 2:
+
+输入: [1,2,3,4]
+输出: false
+示例 3:
+
+输入: [1,1,1,3,3,4,3,2,4,2]
+输出: true
+
+**/
+package main
+
+import (
+	"fmt"
+)
+
+func containsDuplicate(nums []int) bool {
+	M := make(map[int]bool, len(nums))
+	for idx, _ := range nums {
+		CurrentData := nums[idx]
+		if _, ok := M[CurrentData]; ok {
+			return true
+		}
+		M[CurrentData] = true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 1}))
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 4}))
+	fmt.Println(containsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}))
+}
