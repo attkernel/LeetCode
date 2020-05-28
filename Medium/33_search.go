c7b8f424f13cff0fb57cf18710bb6a6d841e2a53
attkernel <attkernel@gmail.com>
2020-05-28 17:52:56 +0800

    add 33 779 374 69 704

---
+++ Medium/33_search.go
@@ -0,0 +1,54 @@
+/**
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+
+你可以假设数组中不存在重复的元素。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+示例 1:
+
+输入: nums = [4,5,6,7,0,1,2], target = 0
+输出: 4
+示例 2:
+
+输入: nums = [4,5,6,7,0,1,2], target = 3
+输出: -1
+
+**/
+package main
+
+import (
+	"fmt"
+)
+
+func search(nums []int, target int) int {
+	start, end := 0, len(nums)-1
+	for start <= end {
+		mid := (start + end) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] >= nums[start] {
+			if target >= nums[start] && target < nums[mid] {
+				end = mid - 1
+			} else {
+				start = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[end] {
+				start = mid + 1
+			} else {
+				end = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(search([]int{7, 8, 9, 10, 0, 1, 2, 3, 4, 5, 6}, 10))
+	fmt.Println(search([]int{3, 1}, 1))
+}
