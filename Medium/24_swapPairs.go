a4ba9082a530e14aae9a2eae4f1898c8f0e10a1f
attkernel <attkernel@gmail.com>
2020-05-28 11:36:11 +0800

    add 24

---
+++ Medium/24_swapPairs.go
@@ -0,0 +1,31 @@
+/**
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+**/
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	tmp := head.Next.Next
+	newHead := head.Next
+	head.Next.Next = head
+	head.Next = swapPairs(tmp)
+	return newHead
+}
+
+func main() {}
