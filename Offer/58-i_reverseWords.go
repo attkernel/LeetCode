1bca6e81329051c618d5eeae9112b1db0223cff5
attkernel <attkernel@gmail.com>
2020-06-05 00:09:13 +0800

    add 58-i

---
+++ Offer/58-i_reverseWords.go
@@ -0,0 +1,64 @@
+/*
+输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+
+
+
+示例 1：
+
+输入: "the sky is blue"
+输出: "blue is sky the"
+示例 2：
+
+输入: "  hello world!  "
+输出: "world! hello"
+解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+示例 3：
+
+输入: "a good   example"
+输出: "example good a"
+解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+
+
+说明：
+
+无空格字符构成一个单词。
+输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func reverseWords(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	res := make([]byte, 0)
+	pStart, pEnd, sl, l := 0, 0, []byte(s), len(s)-1
+	for l >= 0 {
+		for l >= 0 && sl[l] == ' ' {
+			l--
+		}
+		pEnd = l
+		for l >= 0 && sl[l] != ' ' {
+			l--
+		}
+		pStart = l + 1
+		if pStart == pEnd+1 {
+			break
+		}
+		res = append(res, sl[pStart:pEnd+1]...)
+		res = append(res, ' ')
+	}
+	if len(res) != 0 {
+		return string(res[:len(res)-1])
+	}
+	return ""
+}
+
+func main() {
+	fmt.Println(reverseWords("  hello world!  "))
+}
