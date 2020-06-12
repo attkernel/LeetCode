a0b306398f55cc2801cbd44d00e48f1d71d25823
attkernel <attkernel@gmail.com>
2020-06-12 20:51:50 +0800

    add 28

---
+++ Offer/28_isSymmetric.go
@@ -0,0 +1,69 @@
+package main
+
+/*
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+
+
+限制：
+
+0 <= 节点个数 <= 1000
+
+*/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	s := make([]*TreeNode, 0)
+	s = append(s, root.Left)
+	s = append(s, root.Right)
+	for len(s) > 0 {
+		node1 := s[0]
+		node2 := s[1]
+		s = s[2:]
+		if node1 == nil && node2 == nil {
+			continue
+		}
+		if node1 == nil || node2 == nil || node1.Val != node2.Val {
+			return false
+		}
+		s = append(s, node1.Left)
+		s = append(s, node2.Right)
+		s = append(s, node1.Right)
+		s = append(s, node2.Left)
+	}
+	return true
+}
+
+func main() {}
