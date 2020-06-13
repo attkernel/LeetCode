41afdcba29ea4d37ddb86c255b2aa38d93e9d5a5
attkernel <attkernel@gmail.com>
2020-06-13 23:25:41 +0800

    add 27 226

---
+++ Simple/226_invertTree.go
@@ -0,0 +1,52 @@
+/*
+翻转一棵二叉树。
+
+示例：
+
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+*/
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[i]
+			node.Left, node.Right = node.Right, node.Left
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		queue = queue[size:]
+	}
+	return root
+}
+
+func main() {}
