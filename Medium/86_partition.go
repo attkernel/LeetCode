ec25e4a841472f93eb6d92b720d5f155066b23da
attkernel <attkernel@gmail.com>
2020-07-15 23:49:18 +0800

    add 86

---
+++ Medium/86_partition.go
@@ -0,0 +1,40 @@
+/*
+给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+
+你应当保留两个分区中每个节点的初始相对位置。
+
+示例:
+
+输入: head = 1->4->3->2->5->2, x = 3
+输出: 1->2->2->4->3->5
+
+*/
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func partition(head *ListNode, x int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	left, right := new(ListNode), new(ListNode)
+	l, r := left, right
+	for head != nil {
+		if head.Val < x {
+			left.Next = head
+			left = left.Next
+		} else {
+			right.Next = head
+			right = right.Next
+		}
+		head = head.Next
+	}
+	right.Next = nil
+	left.Next = r.Next
+	return l.Next
+}
+
+func main() {}
