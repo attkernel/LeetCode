a65dbd5c070807c29e4173799943cd45749c0c46
attkernel <attkernel@gmail.com>
2020-08-08 23:00:17 +0800

    add 986

---
+++ Medium/986_intervalIntersection.go
@@ -0,0 +1,58 @@
+/*
+给定两个由一些 闭区间 组成的列表，每个区间列表都是成对不相交的，并且已经排序。
+
+返回这两个区间列表的交集。
+
+（形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <= b。两个闭区间的交集是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3]。）
+
+
+
+示例：
+
+
+
+输入：A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+输出：[[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+
+提示：
+
+0 <= A.length < 1000
+0 <= B.length < 1000
+0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+
+*/
+package main
+
+func intervalIntersection(A [][]int, B [][]int) [][]int {
+	var res [][]int
+	i, j := 0, 0
+	for i < len(A) && j < len(B) {
+		if A[i][1] > B[j][1] {
+			if A[i][0] <= B[j][1] {
+				res = append(res, []int{max(A[i][0], B[j][0]), B[j][1]})
+			}
+			j++
+		} else if A[i][1] == B[j][1] {
+			res = append(res, []int{max(A[i][0], B[j][0]), A[i][1]})
+			i++
+			j++
+		} else {
+			if B[j][0] <= A[i][1] {
+				res = append(res, []int{max(A[i][0], B[j][0]), A[i][1]})
+			}
+			i++
+		}
+	}
+	return res
+}
+
+func max(i int, j int) int {
+	if i > j {
+		return i
+	} else {
+		return j
+	}
+}
+
+func main() {}
